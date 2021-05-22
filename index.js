31e1926328ca76edccb3b7177849b90a119251f7
Juan Esteban Angulo Uribe <jangulorama@outlook.com>
2021-05-22 17:13:47 -0500

    .

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hola mundo!')
+console.log('Hola mundo!');
+const valorCOntante="H1";
+const myevent=()=>{
+
+};
+
+// valorCOntante="H2";
+
+console.log('Hola mundo!');
+var i = 15;
+
+function myVarFunction(){
+    let a= 18;
+    console.log(a);
+    if(true){
+        let a=5;
+        console.log(a);
+    }
+
+    console.log(a);
+}
+
+myVarFunction();
+
+// console.log(i);
+
+// (function myVarFunction() {
+//     for (i = 0; i < 8; i++) {
+//         console.log(i);
+//     }
+// })();
+
+// console.log(i);
