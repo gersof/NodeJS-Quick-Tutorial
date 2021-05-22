71a53a96231a88b510830b469c549fcebc2e9180
Juan Esteban Angulo Uribe <jangulorama@outlook.com>
2021-05-22 17:27:55 -0500

    Servidor

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo!')
+const http=require('http');
+const host= 'localhost';
+const port=4141;
+
+const requestListener= function(req, res) {
+    res.writeHead(200);
+    res.end('Hola mundo');
+}
+
+
+const server= http.createServer(requestListener);
+
+server.listen(port,host, ()=>{
+    console.log(`Nuestro servidor esta  corriendo en el puerto ${port} con el host ${host}`);
+
+})
